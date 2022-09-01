0cdad89007743acad53a3f1a7ce15a636148717b
xixo-tan <lilo93choco@icloud.com>
2022-09-01 17:11:47 +0900

    first commit (end of ex03)

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function ($) {
+  $('.bg-switcher').bgSwitcher({
+    images: ['images/bg1.jpg', 'images/bg2.jpg', 'images/bg3.jpg'],
+    interval: 5000,
+    loop: true
+  });
+
+  $(window).scroll(function () {
+    var targetElement = $('.fadein').offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll > targetElement - windowHeight + 200) {
+      $('.fadein').css('opacity', '1');
+      $('.fadein').css('transform', 'translateY(0)');
+    }
+  });
+
+  $('.slider').slick({
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    centerMode: true,
+    centerPadding: '20%',
+  });
+
+});
